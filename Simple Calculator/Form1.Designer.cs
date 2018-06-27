d81c78a25d07c6790b6d9161c4948ae75e8b79ea
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2018-06-27 17:05:01 +0530

    Add files via upload

---
+++ Simple Calculator/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.ans = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // n1
+            // 
+            this.n1.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(1, 53);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(75, 41);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = true;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.Location = new System.Drawing.Point(82, 53);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(75, 41);
+            this.n2.TabIndex = 2;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = true;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n3
+            // 
+            this.n3.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.Location = new System.Drawing.Point(163, 53);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(75, 41);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = true;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // n6
+            // 
+            this.n6.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.Location = new System.Drawing.Point(163, 114);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(75, 41);
+            this.n6.TabIndex = 6;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = true;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n5
+            // 
+            this.n5.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.Location = new System.Drawing.Point(82, 114);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(75, 41);
+            this.n5.TabIndex = 5;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = true;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n4
+            // 
+            this.n4.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.Location = new System.Drawing.Point(1, 114);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(75, 41);
+            this.n4.TabIndex = 4;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = true;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n9
+            // 
+            this.n9.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n9.Location = new System.Drawing.Point(163, 177);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(75, 41);
+            this.n9.TabIndex = 9;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = true;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n8
+            // 
+            this.n8.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n8.Location = new System.Drawing.Point(82, 177);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(75, 41);
+            this.n8.TabIndex = 8;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = true;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n7
+            // 
+            this.n7.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.Location = new System.Drawing.Point(1, 177);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(75, 41);
+            this.n7.TabIndex = 7;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = true;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // mul
+            // 
+            this.mul.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mul.Location = new System.Drawing.Point(255, 147);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(75, 41);
+            this.mul.TabIndex = 12;
+            this.mul.Text = "*";
+            this.mul.UseVisualStyleBackColor = true;
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // sub
+            // 
+            this.sub.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sub.Location = new System.Drawing.Point(255, 100);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(75, 41);
+            this.sub.TabIndex = 11;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(255, 53);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 41);
+            this.add.TabIndex = 10;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // div
+            // 
+            this.div.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.div.Location = new System.Drawing.Point(254, 195);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(75, 41);
+            this.div.TabIndex = 14;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // ans
+            // 
+            this.ans.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ans.Location = new System.Drawing.Point(254, 242);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(75, 41);
+            this.ans.TabIndex = 13;
+            this.ans.Text = "=";
+            this.ans.UseVisualStyleBackColor = true;
+            this.ans.Click += new System.EventHandler(this.ans_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(82, 224);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(145, 41);
+            this.clear.TabIndex = 15;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // n0
+            // 
+            this.n0.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n0.Location = new System.Drawing.Point(1, 224);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(75, 41);
+            this.n0.TabIndex = 16;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = true;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 286);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(342, 22);
+            this.statusStrip1.TabIndex = 17;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(77, 17);
+            this.toolStripStatusLabel1.Text = "Create by me";
+            this.toolStripStatusLabel1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(342, 308);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button ans;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+    }
+}
+
---
+++ Simple Calculator/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double num1,num2;
+        String work;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null) //Checks whether if it is the first letter
+                textBox1.Text = "1";  //Replaces it with the button pressed
+            else
+                textBox1.Text += "1";   //adds it along with the element present
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "2";
+            else
+                textBox1.Text += "2";
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "3";
+            else
+                textBox1.Text += "3";
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "4";
+            else
+                textBox1.Text += "4";
+        }
+
+        private void n5_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "5";
+            else
+                textBox1.Text += "5";
+        }
+
+        private void n6_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "6";
+            else
+                textBox1.Text += "6";
+        }
+
+        private void n7_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "7";
+            else
+                textBox1.Text += "7";
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "8";
+            else
+                textBox1.Text += "8";
+
+        }
+
+        private void n9_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "9";
+            else
+                textBox1.Text += "9";
+        }
+
+        private void n0_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+                textBox1.Text = "0";
+            else
+                textBox1.Text += "0";
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            num1 = 0;
+            num2 = 0;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            work = "+";
+            textBox1.Text = "0";
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+
+            num1 = Convert.ToDouble(textBox1.Text);
+            work = "-";
+            textBox1.Text = "0";
+        }
+
+        private void mul_Click(object sender, EventArgs e)
+        {
+
+            num1 = Convert.ToDouble(textBox1.Text);
+            work = "*";
+            textBox1.Text = "0";
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            work = "/";
+            textBox1.Text = "0";
+        }
+
+        private void ans_Click(object sender, EventArgs e)
+        {
+            double result;
+            num2 = Convert.ToDouble(textBox1.Text);
+            switch (work)
+            {
+                case "+":
+                    {
+                        result = num1 + num2;
+                        textBox1.Text = Convert.ToString(result);//Display Result by converting it to string
+                        num1 = result; break;  //For further processing
+                    }
+                case "-":
+                    {
+                        result = num1 - num2;
+                        textBox1.Text = Convert.ToString(result);//Display Result by converting it to string
+                        num1 = result; break;  //For further processing
+                    }
+                case "*":
+                    {
+                        result = num1 * num2;
+                        textBox1.Text = Convert.ToString(result);//Display Result by converting it to string
+                        num1 = result; break;  //For further processing
+                    }
+                case "/":
+                    {
+                           if (num2 == 0)
+                            textBox1.Text = "by zero division error, change the second value";
+                        result = num1 / num2;
+                        textBox1.Text = Convert.ToString(result);//Display Result by converting it to string
+                        num1 = result; break;  //For further processing
+                    }
+            }
+        }
+    }
+}
